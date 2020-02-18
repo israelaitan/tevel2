f6c641b8e8aba0532a9aa39f59b799b7c807fefd
Assaf Dagai <53687090+assafdag@users.noreply.github.com>
2020-02-18 17:51:26 +0200

    Update ActUponCommand.c

--- GlobusSatProject/src/SubSystemModules/Communication/ActUponCommand.c
+++ GlobusSatProject/src/SubSystemModules/Communication/ActUponCommand.c
@@ -1,6 +1,6 @@
 #include <string.h>
 #include <hal/errors.h>
-
+#include "SPL.h"
 #include "TRXVU.h"
 #include "ActUponCommand.h"
 #include "CommandDictionary.h"
@@ -9,31 +9,34 @@
 int ActUponCommand(sat_packet_t *cmd)
 {
 	int err = 0;
-	if (NULL == cmd){
-		return E_NOT_INITIALIZED;
+
+	//TODO: check if cmd is NULL
+
+	if(cmd->cmd_type==trxvu_cmd_type)
+	{
+		err=trxvu_command_router(cmd);
+	}
+	else if(cmd->cmd_type==eps_cmd_type)
+	{
+		err=eps_command_router(cmd);
+	}
+	else if(cmd->cmd_type==telemetry_cmd_type)
+	{
+		err=telemetry_command_router(cmd);
+	}
+	else if(cmd->cmd_type==filesystem_cmd_type)
+	{
+		 err=filesystem_command_router(cmd);
+	}
+	else if(cmd->cmd_type==managment_cmd_type)
+	{
+		 err=managment_command_router(cmd);
+	}
+	else if(cmd->cmd_type==ack_type)
+	{
+		//TODO: SendAckPacket with ACK_PING
 	}
 
-	switch ((spl_command_type) cmd->cmd_type)
-	{
-	case trxvu_cmd_type:
-		err = trxvu_command_router(cmd);
-		break;
-	case eps_cmd_type:
-		err = eps_command_router(cmd);
-		break;
-	case telemetry_cmd_type:
-		err = telemetry_command_router(cmd);
-		break;
-	case filesystem_cmd_type:
-		err = filesystem_command_router(cmd);
-		break;
-	case managment_cmd_type:
-		err = managment_command_router(cmd);
-		break;
-	case ack_type: 								//this command is a ping function
-		SendAckPacket(ACK_PING, cmd,NULL,0);
-		break;
-	}
 	return err;
 }
 
