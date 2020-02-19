9b03b2006ca82f6bd67aee603d02772163815f14
Assaf Dagai <53687090+assafdag@users.noreply.github.com>
2020-02-19 12:26:57 +0200

    Update InitSystem.c
    Add GS code

--- GlobusSatProject/src/InitSystem.c
+++ GlobusSatProject/src/InitSystem.c
@@ -7,212 +7,283 @@
 #include <at91/utility/exithandler.h>
 #include <string.h>
 #include "GlobalStandards.h"
+#include "InitSystem.h"
 #include "SubSystemModules/PowerManagment/EPS.h"
 #include "SubSystemModules/Communication/TRXVU.h"
 #include "SubSystemModules/Maintenance/Maintenance.h"
 #include "SubSystemModules/Housekepping/TelemetryCollector.h"
-#include "SubSystemModules/Housekepping/Dump.h"
-#include "InitSystem.h"
+#include <satellite-subsystems/IsisAntS.h>
 #include "TLM_management.h"
 
 #ifdef GOMEPS
-#include <satellite-subsystems/GomEPS.h>
+	#include <satellite-subsystems/GomEPS.h>
 #endif
 #ifdef ISISEPS
-#include <satellite-subsystems/IsisEPS.h>
+	#include <satellite-subsystems/IsisEPS.h>
 #endif
-
 #define I2c_SPEED_Hz 100000
 #define I2c_Timeout 10
+#define I2c_TimeoutTest portMAX_DELAY
+#define PRINT_IF_ERR(method) if(0 != err)printf("error in '" #method  "' err = %d\n",err);
 
+//האם זו האינטרקציה הראשונה
 Boolean isFirstActivation()
 {
-	Boolean flag = FALSE;
-	FRAM_read((unsigned char*) &flag, FIRST_ACTIVATION_FLAG_ADDR,
-	FIRST_ACTIVATION_FLAG_SIZE);
-	return flag;
+	unsigned char FirstActivation=0;
+	int res=0;
+#ifdef TESTING
+	printf("IsFirstActivation here");
+#else
+	res = FRAM_read(&FirstActivation,FIRST_ACTIVATION_FLAG_ADDR, FIRST_ACTIVATION_FLAG_SIZE );
+#endif
+	 if (res==-2)
+	 {
+		 printf(" specified address and size are out of range of the FRAM space");
+	 }
+	 if  (res==-1)
+	 {
+		 printf(" obtaining lock for FRAM access fails");
+	 }
+
+	 if (FirstActivation==1)
+	 {
+		 return TRUE;
+	 }
+	 else
+	 {
+		 return FALSE;
+	 }
 }
 
+//בדיקה שעברו 30 דק מהשיגור. טיפול במקרה שיש אתחול חוזר של המערכת
 void firstActivationProcedure()
 {
-#ifdef ISISEPS
-	ieps_statcmd_t eps_cmd;
-#endif
-
 	int err = 0;
+	const int TotalWaitTime = 1000 * 60 * 30 / portTICK_RATE_MS;
+	int AwaitedTime = 0;
 
-	time_unix seconds_since_deploy = 0;
-	err = FRAM_read((unsigned char*) seconds_since_deploy,
-			SECONDS_SINCE_DEPLOY_ADDR,
-			SECONDS_SINCE_DEPLOY_SIZE);
-	if (0 != err) {
-		seconds_since_deploy = MINUTES_TO_SECONDS(30);	// deploy immediately. No mercy
-	}
+#ifdef TESTING
+	printf("FirstActivationProcedure here");
+#else
+	err = FRAM_read ((unsigned char *)&AwaitedTime ,MOST_UPDATED_SAT_TIME_ADDR , MOST_UPDATED_SAT_TIME_SIZE	 );
+#endif
+	if (!err)
+	{
+		while (TotalWaitTime>AwaitedTime)
+		{
 
-	while (seconds_since_deploy < MINUTES_TO_SECONDS(30)) {
-		vTaskDelay(SECONDS_TO_TICKS(10));
-
-		FRAM_write((unsigned char*)&seconds_since_deploy, SECONDS_SINCE_DEPLOY_ADDR,
-				SECONDS_SINCE_DEPLOY_SIZE);
-		if (0 != err) {
-			break;
+			vTaskDelay(1000*10);
+			AwaitedTime += 1000*10;
+#ifdef TESTING
+	printf("inside while after: %d milliseconds\n", AwaitedTime);
+#else
+			FRAM_write(&AwaitedTime , MOST_UPDATED_SAT_TIME_ADDR , MOST_UPDATED_SAT_TIME_SIZE);
+			TelemetryCollectorLogic();
+#endif
 		}
-		TelemetryCollectorLogic();
-
-		seconds_since_deploy += 10;
-
-		//TODO: add more to this...
-#ifdef ISISEPS
-		IsisEPS_resetWDT(EPS_I2C_BUS_INDEX, &eps_cmd);
-#endif
-#ifdef GOMEPS
-		GomEpsResetWDT(EPS_I2C_BUS_INDEX);
-
-#endif
 	}
-
-#ifndef TESTING
-	IsisAntS_autoDeployment(0, isisants_sideA, 10);
-	IsisAntS_autoDeployment(0, isisants_sideB, 10);
-#endif
-	//TODO: log
 }
 
+	//שמירת ערכי ברירת מחדל בזיכרון.
 void WriteDefaultValuesToFRAM()
 {
-	//TODO: write to FRAM all default values (like threshold voltages...)
-
-	time_unix default_no_comm_thresh = DEFAULT_NO_COMM_WDT_KICK_TIME;
-	FRAM_write((unsigned char*) &default_no_comm_thresh,
-			NO_COMM_WDT_KICK_TIME_ADDR,
-			NO_COMM_WDT_KICK_TIME_SIZE);
-
-	EpsThreshVolt_t def_thresh_volt = { .raw = DEFAULT_EPS_THRESHOLD_VOLTAGES};
-	FRAM_write((unsigned char*)def_thresh_volt.raw, EPS_THRESH_VOLTAGES_ADDR,
-	EPS_THRESH_VOLTAGES_SIZE);
-
-	float def_alpha = DEFAULT_ALPHA_VALUE;
-	FRAM_write((unsigned char*) &def_alpha, EPS_ALPHA_FILTER_VALUE_ADDR,
-	EPS_ALPHA_FILTER_VALUE_SIZE);
-
-	time_unix tlm_save_period = 0;
-	tlm_save_period = DEFAULT_EPS_SAVE_TLM_TIME;
-	FRAM_write((unsigned char*) &tlm_save_period, EPS_SAVE_TLM_PERIOD_ADDR,
-			sizeof(tlm_save_period));
-
-	tlm_save_period = DEFAULT_TRXVU_SAVE_TLM_TIME;
-	FRAM_write((unsigned char*) &tlm_save_period, TRXVU_SAVE_TLM_PERIOD_ADDR,
-			sizeof(tlm_save_period));
-
-	tlm_save_period = DEFAULT_ANT_SAVE_TLM_TIME;
-	FRAM_write((unsigned char*) &tlm_save_period, ANT_SAVE_TLM_PERIOD_ADDR,
-			sizeof(tlm_save_period));
-
-	tlm_save_period = DEFAULT_SOLAR_SAVE_TLM_TIME;
-	FRAM_write((unsigned char*) &tlm_save_period, SOLAR_SAVE_TLM_PERIOD_ADDR,
-			sizeof(tlm_save_period));
-
-	tlm_save_period = DEFAULT_WOD_SAVE_TLM_TIME;
-	FRAM_write((unsigned char*) &tlm_save_period, WOD_SAVE_TLM_PERIOD_ADDR,
-			sizeof(tlm_save_period));
-
-	time_unix beacon_interval = 0;
-	beacon_interval = DEFAULT_BEACON_INTERVAL_TIME;
-	FRAM_write((unsigned char*) &beacon_interval, BEACON_INTERVAL_TIME_ADDR,
-			BEACON_INTERVAL_TIME_SIZE);
-
+#ifdef TESTING
+	printf("WriteDefaultValuesToFRAM here");
+#else
+	int DefNoCom=DEFAULT_NO_COMM_WDT_KICK_TIME;
+	FRAM_write(&DefNoCom, NO_COMM_WDT_KICK_TIME_ADDR,sizeof(DefNoCom));
+	int NumberVoltages=NUMBER_OF_THRESHOLD_VOLTAGES;
+	FRAM_write(&NumberVoltages,EPS_THRESH_VOLTAGES_ADDR,EPS_THRESH_VOLTAGES_SIZE);
+	int alpha=DEFAULT_ALPHA_VALUE;
+	FRAM_write(&alpha,EPS_ALPHA_FILTER_VALUE_ADDR,EPS_ALPHA_FILTER_VALUE_SIZE);
+	int eps= DEFAULT_EPS_SAVE_TLM_TIME;
+	FRAM_write(&eps,EPS_SAVE_TLM_PERIOD_ADDR,sizeof(eps));
+	int trxvu=DEFAULT_TRXVU_SAVE_TLM_TIME;
+	FRAM_write(&trxvu,TRXVU_SAVE_TLM_PERIOD_ADDR,sizeof(trxvu));
+	int ant=DEFAULT_ANT_SAVE_TLM_TIME;
+	FRAM_write(&ant, ANT_SAVE_TLM_PERIOD_ADDR,sizeof(ant));
+	int solar=DEFAULT_SOLAR_SAVE_TLM_TIME;
+	FRAM_write(&solar,SOLAR_SAVE_TLM_PERIOD_ADDR,sizeof(solar));
+	int wod=DEFAULT_WOD_SAVE_TLM_TIME;
+	FRAM_write(&wod,WOD_SAVE_TLM_PERIOD_ADDR,sizeof(wod));
+	//TODO:FRAM_write(add beacon);
+#endif
 }
 
+	//אתחול ה FRAM
 int StartFRAM()
 {
-	int error = FRAM_start();
-	return error;
+	int result=0;
+#ifdef TESTING
+	printf("start time here");
+#else
+	result=FRAM_start();
+#endif
+	if(-1==result)
+	{
+		printf("failed to creaT semaforeS");}
+
+	else if(-2==result)
+
+		printf("failed to initializing SPI");
+
+	return result;
 }
 
+	// i2c אתחול ה
 int StartI2C()
 {
-	int error = I2C_start(I2c_SPEED_Hz, I2c_Timeout);
-	return error;
+	int result=0;
+#ifdef TESTING
+	printf("start i2c here");
+#else
+
+	result=I2C_start(I2c_Timeout,I2c_Timeout);
+#endif
+	if(result==-3)
+	{
+		printf("the driver uses a timeout of 1");
+	}
+	else if(result==-2)
+	{
+		printf("TWI peripheral fails");
+	}
+	else if(result==-1)
+	{
+		printf("creating the task that consumes I2C transfer requests failed");
+	}
+
+	if(result==0)
+	{
+		printf("success");}
+		return result;
 }
 
+	//spI אתחול ה
 int StartSPI()
 {
-	int error = SPI_start(bus1_spi, slave1_spi);
-	return error;
+
+	int result= 0;
+
+#ifdef TESTING
+	printf("start spI here");
+#else
+
+	result = SPI_start(bus1_spi, slave1_spi);
+#endif
+
+	if(result==0)
+	{
+		printf("success");
+	}
+	else if(result==-1)
+	{
+		printf("error");
+	}
+	return result;
 }
 
+	//אתחול השעון של הלווין
 int StartTIME()
 {
-	int error = 0;
+#ifdef TESTING
+	printf("start time here");
+#else
+	int err = 0;
 	Time expected_deploy_time = UNIX_DEPLOY_DATE_JAN_D1_Y2020;
-	error = Time_start(&expected_deploy_time, 0);
-	if (0 != error) {
-		return error;
+	err = Time_start(&expected_deploy_time, 0);
+	if (0 != err)
+	{
+		return err;
 	}
-	time_unix time_before_wakeup = 0;
-	if (!isFirstActivation()) {
-		FRAM_read((unsigned char*) &time_before_wakeup,
-		MOST_UPDATED_SAT_TIME_ADDR, MOST_UPDATED_SAT_TIME_SIZE);
 
+	time_unix time_before_wakeup = 0;
+	if (!isFirstActivation())
+	{
+		FRAM_read((unsigned char*) &time_before_wakeup,
+				MOST_UPDATED_SAT_TIME_ADDR, MOST_UPDATED_SAT_TIME_SIZE);
 		Time_setUnixEpoch(time_before_wakeup);
 	}
+#endif
 	return 0;
 }
 
+	// פריסת אנטנות לאחר 30 דק שקט
 int DeploySystem()
 {
-	Boolean first_activation = isFirstActivation();
-
-	if (first_activation) {
-
+#ifdef TESTING
+		printf(" DeploySystem here");
+#endif
+	if(isFirstActivation())
+	{
 		firstActivationProcedure();
 
-		time_unix deploy_time = 0;
-		Time_getUnixEpoch((unsigned int*)&deploy_time);
-		FRAM_write((unsigned char*) deploy_time, DEPLOYMENT_TIME_ADDR,
-		DEPLOYMENT_TIME_SIZE);
+		unsigned int deployTime;
+		Time_getUnixEpoch(&deployTime);
+		FRAM_write((unsigned char *)&deployTime, DEPLOYMENT_TIME_ADDR, DEPLOYMENT_TIME_SIZE);
 
-		first_activation = FALSE; //TODO: set 'first_activation' to TRUE before launch
-		FRAM_write((unsigned char*) &first_activation,
-		FIRST_ACTIVATION_FLAG_ADDR, FIRST_ACTIVATION_FLAG_SIZE);
+		ISISantsI2Caddress addressab;
+		addressab.addressSideA=0;//TODO: which number
+		addressab.addressSideB=0;//TODO: which number
+		int res=IsisAntS_initialize( &addressab, 1);//לוודא שיש מערכת 1
 
-		WriteDefaultValuesToFRAM();
+		if(res==0)
+		{
+			res=IsisAntS_autoDeployment(ISIS_TRXVU_I2C_BUS_INDEX, isisants_sideA, 10);//TODO: define interval
+		}
+
+		if(res==0)
+			res = IsisAntS_autoDeployment(ISIS_TRXVU_I2C_BUS_INDEX, isisants_sideB, 10);//TODO: define interval
+
+		if(res==0)
+		{
+			Boolean firstactivation= FALSE;
+			FRAM_write((unsigned char *)&firstactivation, FIRST_ACTIVATION_FLAG_ADDR, FIRST_ACTIVATION_FLAG_SIZE );
+		}
 	}
+
 	return 0;
 }
 
-#define PRINT_IF_ERR(method) if(0 != err)printf("error in '" #method  "' err = %d\n",err);
+	//אתחול כלל המערכות
 int InitSubsystems()
 {
-	//TODO: check for return value errors
-	int err = 0;
+
+	int err;
+	err = StartSPI();
+	if (err!=0)
+		return err;
 
 	err = StartI2C();
-	PRINT_IF_ERR(StartI2C)
-
-	err = StartSPI();
-	PRINT_IF_ERR(StartSPI)
+	if (err!=0)
+		return err;
 
 	err = StartFRAM();
-	PRINT_IF_ERR(StartFRAM)
+	if (err!=0)
+		return err;
+
+	WriteDefaultValuesToFRAM();
 
 	err = StartTIME();
-	PRINT_IF_ERR(StartTIME)
+	if (err!=0)
+		return err;
 
-	err = InitializeFS(isFirstActivation());
-	PRINT_IF_ERR(InitializeFS)
+	err=InitTrxvu();
+	if (err!=0)
+		return err;
 
-	err = EPS_Init();
-	PRINT_IF_ERR(EPS_Init)
+	err=InitializeFS();
+	if (err!=0)
+		return err;
 
-	err = InitTrxvu();
-	PRINT_IF_ERR(InitTrxvu)
+	err=EPS_Init();
+	if (err!=0)
 
-	err = InitDump();
-	PRINT_IF_ERR(InitDump)
+		return err;
 
 	err = DeploySystem();
-	PRINT_IF_ERR(DeploySystem)
+	if (err!=0)
+		return err;
+
 	return 0;
 }
-
