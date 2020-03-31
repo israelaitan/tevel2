0e7ffff07f0cf3f7cb814c89c4b788616db1bec5
Assaf Dagai <53687090+assafdag@users.noreply.github.com>
2020-03-31 11:23:42 +0300

    Update Beacon.c
    Add comments and remove unused varaibles and methods handling bitrate change from 9600 to 1200

--- GlobusSatProject/src/SubSystemModules/Communication/Beacon.c
+++ GlobusSatProject/src/SubSystemModules/Communication/Beacon.c
@@ -9,56 +9,31 @@
 
 #include "SubSystemModules/Housekepping/TelemetryCollector.h"
 #include "SubSystemModules/Communication/SatCommandHandler.h"
+#include "SubSystemModules/Maintenance/Maintenance.h"
 
 #include "GlobalStandards.h"
 #include "FRAM_FlightParameters.h"
-
-#include "SubSystemModules/Maintenance/Maintenance.h"
 #include "SPL.h"
 #include "Beacon.h"
 #include "SatDataTx.h"
 
+#define BEACON_ID 0xFFFFFFFF
+
 time_unix g_prev_beacon_time = 0;				// the time at which the previous beacon occured
 time_unix g_beacon_interval_time = 0;
-// marks the current beacon cycle(how many were transmitted before change in baud)
-unsigned char g_current_beacon_period;
-// every 'g_beacon_change_baud_period' beacon will be in 1200Bps and not 9600Bps
-unsigned char g_beacon_change_baud_period;
 
+//Initialize beacon parameters
 void InitBeaconParams()
 {
 	#ifdef TESTING
 		printf("Inside InitBeaconParams()\n");
 	#endif
-	g_beacon_change_baud_period = DEFALUT_BEACON_BITRATE_CYCLE;
-	//TODO: check framread result and handle accordingly
-	FRAM_read((unsigned char*)&g_beacon_change_baud_period, BEACON_BITRATE_CYCLE_ADDR, BEACON_BITRATE_CYCLE_SIZE );
+
+	//TODO: Fix code to initialize to default after FRAM read
 	g_beacon_interval_time = DEFAULT_BEACON_INTERVAL_TIME;
 	FRAM_read((unsigned char*)&g_beacon_interval_time, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE );
-	#ifdef TESTING
-		printf("InitBeaconParams() - setting bitrate to 9600\n");
-	#endif
-	IsisTrxvu_tcSetAx25Bitrate(ISIS_TRXVU_I2C_BUS_INDEX, trxvu_bitrate_9600);
 }
 
-/*//Sets the bitrate to 1200 every third beacon and to 9600 otherwise
-int BeaconSetBitrate()
-{
-	//TODO: remove print after testing complete
-		print("Inside BeaconSetBitrate()\n");
-	int err = 0;
-	if (g_current_beacon_period % g_beacon_change_baud_period == 0) {
-		err = IsisTrxvu_tcSetAx25Bitrate(ISIS_TRXVU_I2C_BUS_INDEX,
-				trxvu_bitrate_1200);
-		g_current_beacon_period = 0;
-	}
-	else {
-		err = IsisTrxvu_tcSetAx25Bitrate(ISIS_TRXVU_I2C_BUS_INDEX,
-				trxvu_bitrate_9600);
-	}
-	g_current_beacon_period++;
-	return err;
-}*/
 
 //Beacon logic code
 void BeaconLogic()
@@ -68,15 +43,26 @@ void BeaconLogic()
 	#endif
 	sat_packet_t packet;
 	WOD_Telemetry_t wod = { 0 };
-	unsigned int id=  0xFFFFFFFF;
+	unsigned int id=  BEACON_ID;
+
+	//check if not on mute and EPS has enough power
 	if(CheckTransmitionAllowed())
 	{
+		//check if time to send beacon based on beacon interval
 		if(CheckExecutionTime(g_prev_beacon_time, g_beacon_interval_time))
 		{
+			//get current Whole Orbit Data
 			GetCurrentWODTelemetry(&wod);
+
+			//create from WOD a packet to send to earth
 			AssembleCommand((unsigned char *)&wod, sizeof(wod), trxvu_cmd_type, BEACON_SUBTYPE, id, 0, &packet);
+
+			// transmit packet to earth
 			TransmitSplPacket(&packet, NULL);
+
+			//set last beacon time
 			Time_getUnixEpoch((unsigned int *)&g_prev_beacon_time);
+
 			#ifdef TESTING
 				printf("beacon sent - id: %d data: %s\n",packet.ID, packet.data );
 			#endif
@@ -94,37 +80,17 @@ void BeaconLogic()
 
 }
 
-int UpdateBeaconBaudCycle(unsigned char cycle)
-{
-	//TODO: remove print after testing complete
-	printf("Inside UpdateBeaconBaudCycle() %c, %c\n" , cycle, DEFALUT_BEACON_BITRATE_CYCLE);
-
-	if (cycle < DEFALUT_BEACON_BITRATE_CYCLE)
-		return E_PARAM_OUTOFBOUNDS;
-	int err = FRAM_write(&cycle, BEACON_BITRATE_CYCLE_ADDR, BEACON_BITRATE_CYCLE_SIZE );
-	if (err!=0)
-	{
-		printf("cycle update to FRAM failed\n");
-		return err;
-	}
-	else
-	{
-		//TODO: remove print after testing complete
-		printf("cycle was updated to FRAM successfully\n");
-	}
-
-	g_beacon_change_baud_period = cycle;
-	return E_NO_SS_ERR;
-}
-
-
+//Update beacon intervals - allows changing this interval from earth
 int UpdateBeaconInterval(time_unix intrvl)
 {
 	//TODO: remove print after testing complete
 	printf("Inside UpdateBeaconInterval() interval: %ld, max: %d, min: %d\n" , intrvl, MAX_BEACON_INTERVAL, MIN_BEACON_INTERVAL);
+
+	//check if interval is in allowed range
 	if(intrvl>MAX_BEACON_INTERVAL|| intrvl<MIN_BEACON_INTERVAL)
 		return E_PARAM_OUTOFBOUNDS;
 
+	//update new interval in FRAM
 	int err = FRAM_write((unsigned char *)&intrvl, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE );
 	if (err!=0)
 	{
@@ -137,17 +103,8 @@ int UpdateBeaconInterval(time_unix intrvl)
 		printf("interval was updated to FRAM successfully\n");
 	}
 
+	//set new interval value
 	g_beacon_interval_time = intrvl;
 	return E_NO_SS_ERR;
 }
 
-/*
-void TestBeacon()
-{
-	for(int i=0;i<NUMBEACON;i++)
-	{
-		Beaconlogic();
-		vTaskDelay(AWATEDTIME);
-	}
-}
-*/
