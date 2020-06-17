1ab6e749d5b8a6bf7c5bc1c1142fdafc83a39c94
Assaf Dagai <53687090+assafdag@users.noreply.github.com>
2020-06-18 00:39:14 +0300

    Revert "Add StartSPI method definition"
    This reverts commit 0e89b2220adc7c2968bce2b34a10ab630e437829.

--- GlobusSatProject/src/InitSystem.h
+++ GlobusSatProject/src/InitSystem.h
@@ -31,8 +31,6 @@ int StartI2C();
  * @brief	Starts the SPI using drivers, and checks for errors
  * @see	SPI.h
  */
-int StartSPI();
-
 
 /*!
  * @brief	Starts the Time module using drivers, and checks for errors.
