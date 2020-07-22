dda3e2876c5afa4024c0234bc628fc87da101ac5
orporat73 <orporat73@gmail.com>
2020-07-22 14:45:44 +0300

    Update Maintenance.c

--- GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
+++ GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
@@ -164,10 +164,5 @@ void Maintenance()
 		restart();
 	}
 
-	//TODO: Add Transponder end Logic:
-	// -Check if in Transponder mode,
-	// - if so, check if transponder mode ended
-	// - if so, turn off transponder
-
 	//TODO: if(current_time < FRAM_sat_time) maybe update 'sat_time' to be 'first_wakeup_'
 }
